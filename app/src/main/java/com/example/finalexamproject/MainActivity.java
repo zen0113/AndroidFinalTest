27379fdea4538ddd1b1d61fec81601891b96b6e1
zen0113 <z_zen@naver.com>
2024-05-25 23:32:39 +0900

    메인, 설문 xml 추가

--- app/src/main/java/com/example/finalexamproject/MainActivity.java
+++ app/src/main/java/com/example/finalexamproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.finalexamproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.main);
+
+        Button btnStart = (Button) findViewById(R.id.start);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent it = new Intent(this, SurveyActivity.class);
+        startActivity(it);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/finalexamproject/SurveyActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalexamproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+
+public class SurveyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.survey);
+
+    }
+}
